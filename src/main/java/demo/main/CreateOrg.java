cb5489b2dc1cf33931307fc1e5301f3ed6f109d9
Vamsinayapamu <vamsi964284@gmail.com>
2025-07-18 18:01:19 +0530

    commit1

---
+++ src/main/java/demo/main/CreateOrg.java
@@ -0,0 +1,10 @@
+package demo.main;
+
+public class CreateOrg {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/test/java/demo/test/CreateContact.java
@@ -0,0 +1,10 @@
+package demo.test;
+
+public class CreateContact {
+
+	public static void main(String[] args) {
+		System.out.println("Hai");
+
+	}
+
+}
